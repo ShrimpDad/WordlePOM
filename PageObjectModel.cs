1148b241a518bdef8177446f4801bc46c68a0ac6
Chris Kelly <Chris.Kelly@skipton.co.uk>
2022-12-21 12:28:25 +0000

    Refactor EnterWord to return a string array of the letter states, store these states into an array within the UnitTest results string array, added some comments

--- PageObjectModel.cs
+++ PageObjectModel.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium;
+﻿
+using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 using SeleniumExtras.WaitHelpers;
@@ -14,45 +15,43 @@ namespace WordlePOM
         List<LetterState> letterStates = new List<LetterState>();
 
         private const string WordleURL = "https://www.nytimes.com/games/wordle/index.html";
+
         IWebDriver WebDriver;
-        public PageObjectModel()
+
+        public PageObjectModel() //this is a constructor, this is a special method that is called by default when the class is as it shares the same name
         {
-            WebDriver = new FirefoxDriver();
+            WebDriver = new FirefoxDriver(); //here we create a WebDriver to be a new FirefoxDriver (We could easily switch this out to chrome here without having to update the rest of the methods here)
         }
 
 
-        public void NavigateToPage()
+        public void NavigateToPage() //this is a method that uses the WordleURL private constant string to navigate to the wordle homepage
         {
             WebDriver.Navigate().GoToUrl(WordleURL);
         }
 
 
-        public void RejectCookies()
+        public void RejectCookies() //this is a method that rejects cookies
         {
-            IWebElement element = WebDriver.FindElement(By.Id("pz-gdpr-btn-reject"));
-            
-            element.Click();
+            IWebElement element = WebDriver.FindElement(By.Id("pz-gdpr-btn-reject")); //this finds the reject button
+
+            element.Click(); //this clicks the reject button
         }
 
 
-        public void CloseHowToPlay()
+        public void CloseHowToPlay() //this is a method that closes the how to play popup
         {
-            IWebElement element = WebDriver.FindElement(By.ClassName("game-icon"));
+            IWebElement element = WebDriver.FindElement(By.ClassName("game-icon")); //this finds the cross button on the popup
 
-            element.Click();
+            element.Click(); //this clicks the cross button
         }
 
-        public void SelectLetter(char letter)
+        public void SelectLetter(char letter) //this is a method that takes a letter, converts it to lowercase and clicks the related letterTile on the page
         {
-            char lowerCaseLetter = Char.ToLower(letter);
-            
-            IWebElement element = WebDriver.FindElement(By.XPath($"//button[text()='{lowerCaseLetter}']"));
+            char lowerCaseLetter = Char.ToLower(letter); //this converts the letter passed in method to a lowercase version of the letter
 
-            //WebDriverWait wait = new WebDriverWait(WebDriver, TimeSpan.FromSeconds(5.00));
+            IWebElement letterTile = WebDriver.FindElement(By.XPath($"//button[text()='{lowerCaseLetter}']")); //this finds the correct tile based on the lowercase letter input
 
-            //wait.Until(ExpectedConditions.ElementToBeClickable(element));
-
-            element.Click();
+            letterTile.Click(); //this clicks the letter we passed into the method
         }
 
         public void SelectEnter()
@@ -69,7 +68,7 @@ namespace WordlePOM
             element.Click();
         }
 
-        public void EnterWord(string word)
+        public string[] EnterWord(string word)
         {
             for (int i = 0; i < 5; i++)
             {
@@ -79,6 +78,17 @@ namespace WordlePOM
             SelectEnter();
 
             System.Threading.Thread.Sleep(5000);
+
+            string[] resultsArray = new string[5];
+
+            GetLetterState();
+
+            for (int i = 0; i < letterStates.Count; i++)
+            {
+                resultsArray[i] = letterStates[i].State;
+            }
+
+            return resultsArray;
         }
 
         public void GetLetterState()
